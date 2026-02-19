acd4f240f06d6dc1a7d6f0df82a1f5c87245d62c
modul10 <alvithops@gmail.com>
2026-02-19 21:19:25 +0700

    update56

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ Route::get('/halo/{nama?}', function ($nama = "Tidak Diketahui") {
     return "Halo, nama : " . $nama;
 });
 
-// Route Biodata
-Route::get('/hai/{nama}', function ($nama) {
-    return 'Halo, <br> ' . $nama;
+
+// Route Biodata (Menggunaakn HTML <br> agar pindah baris)
+Route::get('biodata', function () {
+    return 'nama : Alvitho Sipayung <br> NIM : 255410026 <br> Prodi : Teknik Informatika';
+});
+
+// Route Dengan Dua Parameter (nama dan nilai)
+Route::get('/nilai/{nama}/{nilai}', function ($nama, $nilai) {
+    return 'Halo, ' . [$nama] . ', mendapatkan nilai' . $nilai;
 });
