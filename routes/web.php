416fedb16f8b0ddfbae152d451a8051ca3215134
modul10 <alvithops@gmail.com>
2026-02-19 17:31:55 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Selamat Datang di PayungDev';
+});
+
+// Route statis
+Route::get('/profile', function () {
+    return 'ini adalah halaman profil.';
+});
+
+//Route Dengan Paarameter
+Route::get('/halo/{nama}', function ($nama) {
+    return 'halo, '. $nama;
+});
